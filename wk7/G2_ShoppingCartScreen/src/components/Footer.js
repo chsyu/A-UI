7ab1093563bb3470bd0b91da86b1e52193d8a744
chsyu <chsyu@tea.ntue.edu.tw>
2020-10-02 21:03:49 +0800

    remove some unused content

--- wk7/G2_ShoppingCartScreen/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-    return <footer className="footer">All right reserved.</footer>;
-}
-
-export default Footer;
--- wk7/G2_ShoppingCartScreen/src/contexts/index.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-const DispatchContext = createContext();
-const StateContext = createContext();
-
-export { DispatchContext, StateContext }
