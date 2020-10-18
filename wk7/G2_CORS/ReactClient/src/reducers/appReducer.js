8fa6b3fb59bf9e749958395b3f6e760afbfa4f3e
chsyu <chsyu@tea.ntue.edu.tw>
2020-10-19 07:24:59 +0800

    shopping cart

--- wk7/G2_CORS/ReactClient/src/reducers/appReducer.js
+++ wk7/G2_CORS/ReactClient/src/reducers/appReducer.js
@@ -4,24 +4,7 @@ const initialAppState = {
   open: false,
   products: [],
   product: {},
-  cartItems: [],
-  userSignin: {
-    loading: false,
-    userInfo: {
-      name: "ntuedtd",
-    },
-    error: "",
-  },
-  userRegister: {
-    loading: false,
-    userInfo: {
-      name: "ntuedtd"
-    },
-    error: "",
-  },
 };
-
-let cartItems = {};
 
 const appReducer = (state, action) => {
   switch (action.type) {
@@ -33,35 +16,6 @@ const appReducer = (state, action) => {
       return { ...state, products: action.payload };
     case actionType.SET_PRODUCT_DATA:
       return { ...state, product: action.payload };
-
-    case actionType.USER_SIGNIN_REQUEST:
-      return { ...state, userSignin: { ...state.userSignin, loading: true}};
-    case actionType.USER_SIGNIN_SUCCESS:
-      return { ...state, userSignin: { ...state.userSignin, loading: false, userInfo: action.payload}};
-    case actionType.USER_SIGNIN_FAIL:
-      return { ...state, userSignin: { ...state.userSignin, loading: false, error: action.payload}};
-
-    case actionType.USER_REGISTER_REQUEST:
-      return { loading: true };
-    case actionType.USER_REGISTER_SUCCESS:
-      return { loading: false, userInfo: action.payload };
-    case actionType.USER_REGISTER_FAIL:
-      return { loading: false, error: action.payload };
-
-    case actionType.CART_ADD_ITEM:
-      const item = action.payload;
-      const product = state.cartItems.find((x) => x.product === item.product);
-      if (product) {
-        cartItems = state.cartItems.map((x) =>
-          x.product === product.product ? item : x
-        );
-        return { ...state, cartItems };
-      }
-      cartItems = [...state.cartItems, item];
-      return { ...state, cartItems };
-    case actionType.CART_REMOVE_ITEM:
-      cartItems = state.cartItems.filter((x) => x.product !== action.payload);
-      return { ...state, cartItems };
 
     default:
       return state;
